87219ecaf77becb07dc1e3c3470f7e4f2be955be
sosotatrishvili123 <sosotatrishvili610@gmail.com>
2024-07-05 14:54:54 -0700

    second commit

---
+++ level7/homework/level7.py
@@ -0,0 +1,72 @@
+#1) მომხმარებელს შემოატანინეთ 4 სხავდასხვა რიცხვი და ამ ოთხივე რიცხვზე მოახდინეთ არითმეტიკური ოპერაციები. +,-,*,//,**
+
+#num1 = int(input('enter first num:  '))
+#num2 = int(input('enter second num:  '))
+#num3 = int(input('enter third num:  '))
+#num4 = int(input('enter fourth num:  '))
+
+
+
+#======================================================================================================
+#2) დაწერეთ პროგრამა სადაც შემოიტანთ თქვენი ოჯახის წევრების ასაკს და გამოიტანეთ ის თუ რამდენი წლის იქნებიან 20 წლის შემდეგ.
+
+#dad_age = int(input('enter dad age:  '))
+#after_20_years = dad_age + 20
+#print('dad age after 20 years, will be', after_20_years )
+
+#mom_age = int(input('enter  mom age:  '))
+#after_20_years = mom_age + 20
+#print('mom age after 20 years, will be', after_20_years )
+
+#sister_age = int(input(' enter sister age:  '))
+#after_20_years = sister_age + 20
+#print('sister age after 20 years, will be',after_20_years)
+
+#grandfather_age = int(input('enter grandfather_age:  '))
+#after_20_years = grandfather_age + 20
+#print('grandfather after 20 years will be', after_20_years)
+      
+#grandmother_age = int(input('enter grandmother_age:  '))
+#after_20_years = grandmother_age + 20
+#print('grandmother age ater 20 years will be', after_20_years)
+
+
+#=======================================
+#=======================================
+#=======================================
+
+
+#შექმენით ცვლადები სადაც შეინახავთ თქვენს თავზე ინფორმაციას
+#(სახელი, გვარი, ასაკი, ქვეყანა, ქალაქი, საყვარელი ფერი, საყვარელი მანქანა, საყვარელი საჭმელი, საყვარელი სპორტი)
+#შემდეგ შემოტანილი მნიშვნელობებით ააწყვეტ ერთი დიდი წინადადება.
+
+name = input('enter ur name:  ')
+last_name = input('enter ur last name:  ')
+age = input('enter ur age:  ')
+country = input('which country do u live:  ')
+color = input('enter ur fav color:  ')
+car = input('enter ur fav car:  ')
+food = input('enter ur fav food:  ')
+sport = input('enter fav sport:  ')
+  
+
+print("hello,'my name is ' + name 'my last name ' + last_name 'my age ' + age 'i live in ' + country 'fav color is ' + color 'my fav car is ' + car 'my fav food is ' + food 'my fav sport is ' + sport")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
