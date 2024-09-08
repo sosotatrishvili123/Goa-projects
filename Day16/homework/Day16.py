6d1b30bdf859661b3952e367a5cb1f3f736584d6
sosotatrishvili123 <sosotatrishvili610@gmail.com>
2024-09-08 15:55:05 -0700

    second

---
+++ Day16/homework/Day16.py
@@ -0,0 +1,38 @@
+for i in range (30 + 1):
+    print(i)
+
+for x in range (10):
+    print(x)
+
+for g in range (20 + 1):
+    print(g)
+
+
+for h in range (40 + 1):
+    print(h)
+
+for u in range (30 + 1):
+    print(u)
+
+
+for d in range (50 + 1): 
+    print(d)
+
+for q in range (60 + 1):
+    print(q)
+
+for t in range (25 + 1):
+    print(t)
+
+for p in range (100 + 1):
+    print(p)
+
+for a in range (300 + 1):
+    print(a)
+    
+for i in range (1, 10 + 1, 2):
+    print(i)
+
+
+for o in range (0, 10 + 1, 2):
+    print(o)
